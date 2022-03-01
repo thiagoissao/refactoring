7e2be616a7498568a59829f277732f3150027090
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2022-03-01 06:53:22 -0300

    Criação de classses e subclasses

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -1,6 +1,46 @@
 const fs = require('fs');
 const plays = JSON.parse(fs.readFileSync('./data/plays.json'));
 
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  getAmount() {
+    throw new Error(`subclass responsibility`);
+  }
+
+  getVolumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  getAmount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  getAmount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 1000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.getVolumeCredits() + Math.floor(this.performance.audience / 5);
+  }
+}
+
 function totalVolumeCredits(data) {
   return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 }
@@ -9,47 +49,31 @@ function totalAmount(data) {
   return data.performances.reduce((total, p) => total + p.amount, 0);
 }
 
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 1000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ('comedy' === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
 }
 
 function enrichPerformance(aPerformance) {
+  const calculator = createPerformanceCalculator(
+    aPerformance,
+    playFor(aPerformance)
+  );
   const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
+  result.play = calculator.play;
+  result.amount = calculator.getAmount();
+  result.volumeCredits = calculator.getVolumeCredits();
   return result;
 }
 
@@ -61,4 +85,5 @@ function createStatementData(invoice) {
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return statementData;
 }
+
 module.exports = createStatementData;
